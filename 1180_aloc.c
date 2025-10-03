a840afff26b241163ce8b204d0881984ca41f6fb
stDavi <augusto.davi@unifesp.br>
2025-10-03 14:47:49 -0300

    Corrigido formato para o beecrowd

--- 1180_aloc.c
+++ 1180_aloc.c
@@ -1,41 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>  
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    int N, i, *x, menor, posicao;
+int main() {
+    int N, i, menor, posicao;
+    int *vetor;
 
-    printf("Digite o tamanho do seu vetor: ");
     scanf("%d", &N);
 
-    int vetor[N];
+    // aloca dinamicamente um vetor de N inteiros
+    vetor = (int *) malloc(N * sizeof(int));
 
-    x = (int *) malloc(N * sizeof(int));
-
-    if(x){
-        printf("Erro: nao foi possivel alocar memoria\n");
-        return 1;
+    if (vetor == NULL) {
+        return 1; // erro na alocação
     }
 
-    for(i = 0; i < N; i++){
-       printf("Digite o valor de vetor[%d]: ", i);
-       scanf("%%d", &vetor[i]);
-
+    for (i = 0; i < N; i++) {
+        scanf("%d", &vetor[i]);
     }
 
     menor = vetor[0];
-    posicao = 0;    
+    posicao = 0;
 
-    for (i = 1; i < N; i++){
-        if(menor > vetor[i]){
+    for (i = 1; i < N; i++) {
+        if (vetor[i] < menor) {
             menor = vetor[i];
             posicao = i;
         }
     }
 
-    printf("Menor valor: %d\tPosicao do menor valor: %d\n", menor, posicao);
+    printf("Menor valor: %d\n", menor);
+    printf("Posicao: %d\n", posicao);
 
-    free(x);
+    // libera a memória
+    free(vetor);
 
     return 0;
 }
