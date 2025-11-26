fea80c699ecbdbf8e54c8496c9dba09575c02926
stDavi <augusto.davi@unifesp.br>
2025-11-25 21:51:34 -0300

    Exercicio usando mergesort

---
+++ 1566_mergesort.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int *L = (int*) malloc(n1 * sizeof(int));
+    int *R = (int*) malloc(n2 * sizeof(int));
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+
+    while (j < n2)
+        arr[k++] = R[j++];
+
+    free(L);
+    free(R);
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int NC, N;
+    scanf("%d", &NC);
+
+    while (NC--) {
+        scanf("%d", &N);
+
+        int *alturas = (int*) malloc(N * sizeof(int));
+
+        for (int i = 0; i < N; i++) {
+            scanf("%d", &alturas[i]);
+        }
+
+        mergeSort(alturas, 0, N - 1);
+
+        for (int i = 0; i < N; i++) {
+            if (i > 0) printf(" ");
+            printf("%d", alturas[i]);
+        }
+        printf("\n");
+
+        free(alturas);
+    }
+
+    return 0;
+}
