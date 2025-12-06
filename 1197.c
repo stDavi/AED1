28c1e2222f7d342aeee0d2d180e0417ef3a82112
stDavi <augusto.davi@unifesp.br>
2025-12-06 18:46:39 -0300

    Exercício volta à Faculdade de Física

---
+++ 1197.c
@@ -0,0 +1,13 @@
+//Volta à Faculdade de Física
+
+#include <stdio.h>
+
+int main() {
+    int v, t;
+    
+    while (scanf("%d %d", &v, &t) != EOF) {
+        printf("%d\n", v * 2 * t);
+    }
+    
+    return 0;
+}
